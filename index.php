29d38c6b64216c64f295a8ffca1df371a53cea17
Alexander Zemskov <zaismit@gmail.com>
2019-04-25 19:07:18 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+		<script>
+		// number
+		1, 2, 3;
+		// string
+		'string', "string", `string`;
+		// boolean
+		true/false;
+		// null
+		null; 		//does not exist
+		// undefined
+		undefined; 	//is not defined
+		// object
+		let obj = {
+			name:'alex' //key:value
+		}
+		//array
+		let arr = [
+			1,2,3,4    //0,1,2,3
+		]
+		</script>
+	</body>
+</html>
